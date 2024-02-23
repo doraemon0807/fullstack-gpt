a88818299b181820d24f245edb119a579070f08a
doraemon0807 <98371903+doraemon0807@users.noreply.github.com>
2024-02-23 00:22:16 -0800

    documentGPT + streamlit

---
+++ pages/02_QuizGPT.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="QuizGPT", page_icon="🤖")
+
+st.title("QuizGPT")
---
+++ pages/04_SiteGPT.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="SiteGPT", page_icon="🤖")
+
+st.title("SiteGPT")
