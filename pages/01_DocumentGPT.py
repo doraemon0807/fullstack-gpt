281eb6aa390e58ba760ce3d632f24f2ca5cdf3d1
doraemon0807 <98371903+doraemon0807@users.noreply.github.com>
2024-03-13 22:44:38 -0700

    documentGPT final

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,6 +1,9 @@
+from typing import Dict, List
+from uuid import UUID
+from langchain.schema.output import ChatGenerationChunk, GenerationChunk
 import streamlit as st
 
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
@@ -8,12 +11,46 @@ from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
 from langchain.storage import LocalFileStore
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.memory import ConversationSummaryBufferMemory
 
 st.set_page_config(page_title="DocumentGPT", page_icon="🤖")
 
-llm = ChatOpenAI(
-    temperature=0.1,
-)
+
+@st.cache_resource
+def init_llm(chat_callback: bool):
+    if chat_callback == True:  # llm for chat requires callback
+
+        class ChatCallBackHandler(BaseCallbackHandler):
+
+            def __init__(self, *args, **kwargs):
+                self.message = ""
+
+            def on_llm_start(self, *args, **kwargs):  # When llm starts
+                self.message_box = st.empty()  # Create empty box upon llm start
+                self.message = ""  # Initialize message
+
+            def on_llm_new_token(
+                self, token, *args, **kwargs
+            ):  # When llm creates new token
+                self.message += token  # Accumulate tokens into message
+                self.message_box.markdown(self.message)  # Add message to the empty box
+
+            def on_llm_end(self, *args, **kwargs):  # When llm ends
+                save_message(self.message, "ai")  # Save the generated message as AI
+
+        callbacks = [ChatCallBackHandler()]
+    else:  # llm for memory does not require callbacks
+        callbacks = []
+
+    return ChatOpenAI(temperature=0.1, streaming=True, callbacks=callbacks)
+
+
+@st.cache_resource
+def init_memory(_llm):
+    return ConversationSummaryBufferMemory(
+        llm=_llm, max_token_limit=120, return_messages=True, memory_key="chat_history"
+    )
 
 
 # if the file didn't change, this function will not run again
@@ -54,12 +91,17 @@ def embed_file(file):
     return retriever
 
 
+def save_message(message, role):
+    # save new message by appending it to message state
+    st.session_state["messages"].append({"message": message, "role": role})
+
+
 def send_message(message, role, save=True):
+    # Render the chat message on the screen with specified message and role
     with st.chat_message(role):
         st.markdown(message)
     if save:
-        # save new message by appending it to message state
-        st.session_state["messages"].append({"message": message, "role": role})
+        save_message(message, role)
 
 
 def paint_history():
@@ -72,6 +114,22 @@ def format_docs(docs):
     return "\n\n".join(document.page_content for document in docs)
 
 
+def load_memory(_):
+    return memory.load_memory_variables({})["chat_history"]
+
+
+def invoke_chain(question):
+    result = chain.invoke(question)
+    memory.save_context({"input": question}, {"output": result.content})
+
+
+# Creating llm for chat and llm for memory
+llm_for_chat = init_llm(chat_callback=True)
+llm_for_memory = init_llm(chat_callback=False)
+
+# Creating a memory
+memory = init_memory(llm_for_memory)
+
 # Creating the prompt
 prompt = ChatPromptTemplate.from_messages(
     [
@@ -83,9 +141,15 @@ prompt = ChatPromptTemplate.from_messages(
             Context: {context}
             """,
         ),
+        MessagesPlaceholder(variable_name="chat_history"),
         ("human", "{question}"),
     ]
 )
+
+
+# ====================================================================
+# Rendering...
+# ====================================================================
 
 st.title("DocumentGPT")
 
@@ -126,16 +190,18 @@ if file:
                 | RunnableLambda(
                     format_docs
                 ),  # calls retriever(message) -> returns bunch of documents as an output -> calls format_docs(output) -> returns a chunk of joined string
-                "question": RunnablePassthrough(),  # RunnablePassthrough: relays message
+                "question": RunnablePassthrough(),  # RunnablePassthrough: takes the "question" to go straight to the prompt
+                "chat_history": load_memory,
             }
             | prompt  # context and question from the above steps is given to prompt as an input
-            | llm  # prompt from the above step is given to llm as an input
+            | llm_for_chat  # prompt from the above step is given to llm as an input
         )
 
-        response = chain.invoke(message)
-        send_message(response.content, "AI")
+        with st.chat_message("ai"):
+            invoke_chain(message)  # Invoke the chain
 
 
 else:
     # initialize message state if no file is present
     st.session_state["messages"] = []
+    memory.clear()
